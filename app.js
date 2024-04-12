a4a0e50fec5703a3a73b13cbf1252a8f7ea8c177
Athul <athulcm771@gmail.com>
2024-04-12 15:10:31 +0530

    login

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //Connecting with mongodb
-mongoose.connect(process.env.MONGODB)
+mongoose.connect(process.env.MONGODB,)
 .then(() => {
     console.log("Database connected");
 });
